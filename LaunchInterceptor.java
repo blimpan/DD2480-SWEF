3ed8450770b1fea67c53f60ae3b52b83d83a7409
linus <linusbalt3r@gmail.com>
2025-01-25 12:16:28 +0100

    Added function for checking LIC4.

--- LaunchInterceptor.java
+++ LaunchInterceptor.java
@@ -1,3 +1,5 @@
+import java.util.HashSet;
+import java.util.Set;
 
 public class LaunchInterceptor {
 
@@ -128,6 +130,46 @@ public class LaunchInterceptor {
         }
 
         return false; // If program reaches this point, no such triangle exists
+    }
+
+    /**
+     * Determines whether or not there exists at least one set of Q_PTS points that lie in more than QUADS different quadrants.
+     * @return true or false
+     */
+    public Boolean determineLIC4() {
+        if (2 > parameters.Q_PTS || parameters.Q_PTS > numPoints) {
+            return false; // Invalid conditions
+        }
+        if (1 > parameters.QUADS || parameters.QUADS > 3) {
+            return false; // Invalid conditions
+        }
+
+        for (int i = 0; i <= numPoints - parameters.Q_PTS; i++) {
+            Set<Integer> quadrants = new HashSet<>();
+
+            for (int j = 0; j < parameters.Q_PTS; j++) {
+                double xPos = x[i + j];
+                double yPos = y[i + j];
+
+                // Determine the quadrant of the point
+                if (xPos >= 0 && yPos >= 0) {
+                    quadrants.add(1); // Quadrant I
+                } else if (xPos < 0 && yPos >= 0) {
+                    quadrants.add(2); // Quadrant II
+                } else if (xPos <= 0 && yPos < 0) {
+                    quadrants.add(3); // Quadrant III
+                } else if (xPos > 0 && yPos < 0) {
+                    quadrants.add(4); // Quadrant IV
+                }
+
+                // Break early if enough quadrants are found
+                if (quadrants.size() > parameters.QUADS) {
+                    return true;
+                }
+            }
+        }
+
+    return false; // No set meets the condition
     }
 
     public void setInputVariables(int inNumPoints, double[][] inPoints, Parameters inParameters, Connectors[][] inLCM, boolean[] inPUV) {
