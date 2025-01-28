ab975b377f55b0966f67b345f517f6818af5fda6
linus <linusbalt3r@gmail.com>
2025-01-28 11:37:09 +0100

    Re-added N_PTS to record Parameters.

--- LaunchInterceptor.java
+++ LaunchInterceptor.java
@@ -87,7 +87,7 @@ public class LaunchInterceptor {
      */
     public record Parameters(double LENGTH1, double RADIUS1, double EPSILON, double AREA1,
                                     double LENGTH2, double RADIUS2, double AREA2, double DIST,
-                                    int Q_PTS, int QUADS, int K_PTS, int A_PTS, int B_PTS,
+                                    int Q_PTS, int QUADS, int N_PTS, int K_PTS, int A_PTS, int B_PTS,
                                     int C_PTS, int D_PTS, int E_PTS, int F_PTS, int G_PTS){}
 
     /**
