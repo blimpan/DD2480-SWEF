b6ff7d760efa4b0541766d7f436eb6b566ccadb3
KristinRosen <kristin.maria.rosen@gmail.com>
2025-01-29 14:46:37 +0100

    fix: made pointsDistance method public

--- LaunchInterceptor.java
+++ LaunchInterceptor.java
@@ -765,7 +765,7 @@ public class LaunchInterceptor {
     }
 
     //Calculates distance between two points
-    private static double pointsDistance(double x1, double y1, double x2, double y2) {
+    public double pointsDistance(double x1, double y1, double x2, double y2) {
         double distance = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
         return distance;
     }
